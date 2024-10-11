50b34630a2f800ed0f0303901a4a6247a38312cb
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-11 19:09:38 +0200

    Tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe User do
 
   it { is_expected.to validate_presence_of(:email) }
 
-  it { is_expected.to validate_uniqueness_of(:email) }
+  describe "email uniq validations" do
+    subject { create(:user, password: "password", password_digest: nil) }
+
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
 end
