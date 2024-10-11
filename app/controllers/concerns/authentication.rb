7a15066bd1198ef7db712c8a94d7b51130d463ae
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-11 20:56:26 +0200

    Sign in

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,5 +1,23 @@
 module Authentication
+  def require_authentication
+    restore_authentication || request_authentication
+  end
+
+  def restore_authentication
+    User.find_by(id: cookies[:user_id])
+  end
+
+  def request_authentication
+    redirect_to new_session_path, notice: "You must be signed in to perform that action"
+  end
+
+  def restore_authentication
+    Current.user = User.find_by(id: cookies[:user_id])
+  end
+
   # @param user [User]
   def sign_in(user)
+    # Warning: This is not the final implementation and is currently insecure
+    cookies.permanent[:user_id] = user.id
   end
 end
