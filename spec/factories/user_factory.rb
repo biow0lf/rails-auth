2a3ee7fa26ca84bc5fba1447e444389af9108b53
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-11 15:58:42 +0200

    Update user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.unique.email(domain: "example.com") }
+
+    password_digest { BCrypt::Password.create(Faker::Internet.password) }
   end
 end
