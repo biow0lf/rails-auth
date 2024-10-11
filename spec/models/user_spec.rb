fea65b2103f33dd4f3b5ef07b088fbf47fe662d9
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-11 16:18:53 +0200

    Validate password length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe User do
   it { is_expected.to be_an(ApplicationRecord) }
 
   it { is_expected.to have_secure_password }
+
+  it { is_expected.to validate_length_of(:password).is_at_least(8) }
 end
