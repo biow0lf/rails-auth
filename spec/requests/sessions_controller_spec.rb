9695d9fca1e8310e4b8c3e84ba23c78ef4feeb75
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-13 15:57:17 +0200

    More tests

--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -26,4 +26,36 @@ RSpec.describe SessionsController do
       end
     end
   end
+
+  describe "#create" do
+    context "when user is not signed in" do
+      it "is expected to redirect to dashboard path" do
+        user = create(:user, :with_default_password)
+
+        get dashboard_path
+
+        expect(response).to redirect_to(new_session_path)
+
+        _sign_in(user)
+
+        expect(response).to redirect_to(dashboard_path)
+
+        get dashboard_path
+
+        expect(response).to render_template(:show)
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when user is signed in" do
+      it "is expected to redirect to root path" do
+        user = create(:user, :with_default_password)
+
+        2.times { _sign_in(user) }
+
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
