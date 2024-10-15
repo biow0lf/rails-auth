02bf021bb60a585735784ff507b87f5a759de778
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-15 14:52:58 +0200

    Tests

--- spec/requests/password_resets_controller_spec.rb
+++ spec/requests/password_resets_controller_spec.rb
@@ -43,30 +43,32 @@ RSpec.describe PasswordResetsController do
 
   describe "#update" do
     context "when user is not signed in" do
-      it "is expected to redirect to dashboard path" do
-        user = create(:user)
+      context "with valid token and password match password confirmation" do
+        it "is expected to redirect to dashboard path" do
+          user = create(:user)
 
-        token = user.generate_token_for(:password_reset)
+          token = user.generate_token_for(:password_reset)
 
-        patch password_reset_path, params: {
-          password_reset_token: token,
-          password_reset: {
-            password: "newpassword",
-            password_confirmation: "newpassword"
+          patch password_reset_path, params: {
+            password_reset_token: token,
+            password_reset: {
+              password: "newpassword",
+              password_confirmation: "newpassword"
+            }
           }
-        }
 
-        expect(response).to redirect_to(dashboard_path)
+          expect(response).to redirect_to(dashboard_path)
 
-        expect(flash[:notice]).to eq("Password has been successfully reset")
+          expect(flash[:notice]).to eq("Password has been successfully reset")
 
-        get dashboard_path
+          get dashboard_path
 
-        expect(response).to render_template(:show)
+          expect(response).to render_template(:show)
 
-        expect(response).to have_http_status(:ok)
+          expect(response).to have_http_status(:ok)
 
-        expect(user.reload.authenticate("newpassword")).to eq(user)
+          expect(user.reload.authenticate("newpassword")).to eq(user)
+        end
       end
     end
   end
