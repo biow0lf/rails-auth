8580b7c90a18a3812b4295f33f260472cba42490
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-15 14:43:41 +0200

    Tests

--- spec/requests/password_resets_controller_spec.rb
+++ spec/requests/password_resets_controller_spec.rb
@@ -28,4 +28,16 @@ RSpec.describe PasswordResetsController do
       end
     end
   end
+
+  describe "#edit" do
+    context "when user is not signed in" do
+      it "is expected to render template new with status ok" do
+        get edit_password_reset_path
+
+        expect(response).to render_template(:edit)
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
